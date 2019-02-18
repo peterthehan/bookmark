f912fffaff26434e2945724732679afd4a2c0607
Peter Han <peter.han.9493@gmail.com>
2019-02-17 22:54:37 -0500

    Run prettier

--- background.js
+++ background.js
@@ -1,7 +1,4 @@
-const blacklist = [
-  'chrome://bookmarks',
-  'chrome://newtab',
-];
+const blacklist = ['chrome://bookmarks', 'chrome://newtab'];
 
 function isBlacklisted(url) {
   return blacklist.some(i => url.startsWith(i));
@@ -14,36 +11,51 @@ function removeTabs(tabs) {
 function createProperties(resultId, active) {
   return {
     url: `chrome://bookmarks/?id=${resultId}`,
-    active,
+    active
   };
 }
 
 function goToBookmarksManager(tabs, filtered, result) {
   if (tabs.length === 0) {
-    chrome.tabs.create(createProperties(result.id, true), (_) => removeTabs(filtered));
+    chrome.tabs.create(createProperties(result.id, true), _ =>
+      removeTabs(filtered)
+    );
     return;
   }
 
   const [firstTab, ...restTabs] = tabs;
-  chrome.tabs.update(firstTab.id, createProperties(result.id, true), (_) => {
-    chrome.windows.update(firstTab.windowId, { focused: true, }, (_) => removeTabs(filtered));
+  chrome.tabs.update(firstTab.id, createProperties(result.id, true), _ => {
+    chrome.windows.update(firstTab.windowId, { focused: true }, _ =>
+      removeTabs(filtered)
+    );
   });
 
   const notActive = createProperties(result.id, false);
   restTabs.map(tab => chrome.tabs.update(tab.id, notActive));
 }
 
-chrome.browserAction.onClicked.addListener((_) => {
-  chrome.tabs.query({ pinned: false, currentWindow: true, }, (tabs) => {
+chrome.browserAction.onClicked.addListener(_ => {
+  chrome.tabs.query({ pinned: false, currentWindow: true }, tabs => {
     const filtered = tabs.filter(tab => !isBlacklisted(tab.url));
 
     if (filtered.length === 0) {
       return;
     }
 
-    chrome.bookmarks.create({ index: 0, title: new Date().toLocaleString(), }, (result) => {
-      filtered.forEach(tab => chrome.bookmarks.create({ parentId: result.id, title: tab.title, url: tab.url, }));
-      chrome.tabs.query({ url: 'chrome://bookmarks/*', }, tabs => goToBookmarksManager(tabs, filtered, result));
-    });
+    chrome.bookmarks.create(
+      { index: 0, title: new Date().toLocaleString() },
+      result => {
+        filtered.forEach(tab =>
+          chrome.bookmarks.create({
+            parentId: result.id,
+            title: tab.title,
+            url: tab.url
+          })
+        );
+        chrome.tabs.query({ url: 'chrome://bookmarks/*' }, tabs =>
+          goToBookmarksManager(tabs, filtered, result)
+        );
+      }
+    );
   });
 });
