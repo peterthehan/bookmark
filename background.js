d317541f0b57e51cf0f03836707231685e916226
Peter Han <peter.han.9493@gmail.com>
2022-03-19 17:32:18 -0700

    Rewrite using async await, update to manifest v3 api

--- background.js
+++ background.js
@@ -1,61 +1,49 @@
-const blacklist = ['chrome://bookmarks', 'chrome://newtab'];
+const CHROME_SCHEME = "chrome";
 
-function isBlacklisted(url) {
-  return blacklist.some(i => url.startsWith(i));
-}
-
-function removeTabs(tabs) {
-  chrome.tabs.remove(tabs.map(tab => tab.id));
-}
-
-function createProperties(resultId, active) {
-  return {
-    url: `chrome://bookmarks/?id=${resultId}`,
-    active
+function toPromise(api) {
+  return (...args) => {
+    return new Promise((resolve) => {
+      api(...args, resolve);
+    });
   };
 }
 
-function goToBookmarksManager(tabs, filtered, result) {
+const queryTabs = toPromise(chrome.tabs.query);
+const createBookmarks = toPromise(chrome.bookmarks.create);
+const createTabs = toPromise(chrome.tabs.create);
+const removeTabs = toPromise(chrome.tabs.remove);
+
+async function queryValidTabs() {
+  const tabs = await queryTabs({ pinned: false, currentWindow: true });
+  return tabs.filter((tab) => !tab.url.startsWith(CHROME_SCHEME));
+}
+
+async function createBookmarkTree(tabs) {
+  const parent = await createBookmarks({
+    index: 0,
+    title: new Date().toLocaleString(),
+  });
+
+  tabs.forEach((tab) =>
+    createBookmarks({ parentId: parent.id, title: tab.title, url: tab.url })
+  );
+
+  return parent;
+}
+
+chrome.action.onClicked.addListener(async () => {
+  const tabs = await queryValidTabs();
+
   if (tabs.length === 0) {
-    chrome.tabs.create(createProperties(result.id, true), _ =>
-      removeTabs(filtered)
-    );
     return;
   }
 
-  const [firstTab, ...restTabs] = tabs;
-  chrome.tabs.update(firstTab.id, createProperties(result.id, true), _ => {
-    chrome.windows.update(firstTab.windowId, { focused: true }, _ =>
-      removeTabs(filtered)
-    );
+  const parent = await createBookmarkTree(tabs);
+
+  await createTabs({
+    url: `${CHROME_SCHEME}://bookmarks/?id=${parent.id}`,
+    active: true,
   });
 
-  const notActive = createProperties(result.id, false);
-  restTabs.map(tab => chrome.tabs.update(tab.id, notActive));
-}
-
-chrome.browserAction.onClicked.addListener(_ => {
-  chrome.tabs.query({ pinned: false, currentWindow: true }, tabs => {
-    const filtered = tabs.filter(tab => !isBlacklisted(tab.url));
-
-    if (filtered.length === 0) {
-      return;
-    }
-
-    chrome.bookmarks.create(
-      { index: 0, title: new Date().toLocaleString() },
-      result => {
-        filtered.forEach(tab =>
-          chrome.bookmarks.create({
-            parentId: result.id,
-            title: tab.title,
-            url: tab.url
-          })
-        );
-        chrome.tabs.query({ url: 'chrome://bookmarks/*' }, tabs =>
-          goToBookmarksManager(tabs, filtered, result)
-        );
-      }
-    );
-  });
+  removeTabs(tabs.map((tab) => tab.id));
 });
